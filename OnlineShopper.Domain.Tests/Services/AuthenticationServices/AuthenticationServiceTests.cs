8f254242d73affe25cb86f2c280f31d6b588a6ee
abhijith sunder <abhijithsunder01@gmail.com>
2022-04-28 23:50:28 +0530

    Add tests

---
+++ OnlineShopper.Domain.Tests/Services/AuthenticationServices/AuthenticationServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using OnlineShopper.Domain.Models;
+using OnlineShopper.Domain.Services;
+using OnlineShopper.Domain.Services.AuthenticationServices;
+using OnlineShopper.Domain.Services.Facade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopper.Domain.Tests.Services.AuthenticationServices
+{
+    [TestFixture]
+    public class AuthenticationServiceTests
+    {
+        private Mock<IPasswordHasher<User>> _mockPasswordHasher;
+        private Mock<IAccountsService> _mockAccountService;
+        private AuthenticationService _authenticationService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockPasswordHasher = new Mock<IPasswordHasher<User>>();
+            _mockAccountService = new Mock<IAccountsService>();
+            _authenticationService = new AuthenticationService(_mockAccountService.Object, _mockPasswordHasher.Object);
+        }
+
+        [Test]
+        public async Task Login_WithCorrectPasswordForExistingUsername_ReturnsAccountForCorrectUsername()
+        {
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            _mockAccountService.Setup(s => s.GetByUsername(expectedUsername)).ReturnsAsync(new Account() { AccountHolder = new User() { Username = expectedUsername } });
+            _mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<User>(), It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Success);
+
+            Account account = await _authenticationService.Login(expectedUsername, password);
+
+            string actualUsername = account.AccountHolder.Username;
+            Assert.AreEqual(expectedUsername, actualUsername);
+        }
+
+        [Test]
+        public void Login_WithIncorrectPasswordForExistingUsername_ThrowsException()
+        {
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            _mockAccountService.Setup(s => s.GetByUsername(expectedUsername)).ReturnsAsync(new Account() { AccountHolder = new User() { Username = expectedUsername } });
+            _mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<User>(), It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Failed);
+
+            Exception exception = Assert.ThrowsAsync<Exception>(() => _authenticationService.Login(expectedUsername, password));
+
+            Assert.AreEqual("Invalid username or password", exception.Message);
+        }
+
+        [Test]
+        public void Login_WithNonExistingUsername_ThrowsException()
+        {
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            _mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<User>(), It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Failed);
+
+            Exception exception = Assert.ThrowsAsync<Exception>(() => _authenticationService.Login(expectedUsername, password));
+
+            Assert.AreEqual("User not found.", exception.Message);
+        }
+
+        [Test]
+        public async Task Register_WithPasswordsNotMatching_ReturnsPasswordsDoNotMatch()
+        {
+            string password = "testpassword";
+            string confirmPassword = "confirmtestpassword";
+            RegistrationResult expected = RegistrationResult.PasswordsDoNotMatch;
+
+            RegistrationResult actual = await _authenticationService.Register(It.IsAny<string>(), It.IsAny<string>(), password, confirmPassword);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task Register_WithAlreadyExistingEmail_ReturnsEmailAlreadyExists()
+        {
+            string email = "test@gmail.com";
+            _mockAccountService.Setup(s => s.GetByEmail(email)).ReturnsAsync(new Account());
+            RegistrationResult expected = RegistrationResult.EmailAlreadyExists;
+
+            RegistrationResult actual = await _authenticationService.Register(email, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task Register_WithAlreadyExistingUsername_ReturnsUsernameAlreadyExists()
+        {
+            string username = "testuser";
+            _mockAccountService.Setup(s => s.GetByUsername(username)).ReturnsAsync(new Account());
+            RegistrationResult expected = RegistrationResult.UsernameAlreadyExists;
+
+            RegistrationResult actual = await _authenticationService.Register(It.IsAny<string>(), username, It.IsAny<string>(), It.IsAny<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task Register_WithNonExistingUserAndMatchingPasswords_ReturnsSuccess()
+        {
+            RegistrationResult expected = RegistrationResult.Success;
+
+            RegistrationResult actual = await _authenticationService.Register(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
